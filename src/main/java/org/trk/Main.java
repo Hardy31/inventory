8496201bdcc9a483d35af63da5a4eb112981d8fd
Hardy31 <alex-vav@list.ru>
2024-05-07 14:14:15 +0300

    add Spring Boot and JPA

---
+++ src/main/java/org/trk/Main.java
@@ -0,0 +1,31 @@
+package org.trk;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//Добавляем аннотацию
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("INVENTORY");
+    }
+
+
+
+
+//    public static void main(String[] args) {
+//        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//        // to see how IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+//
+////        for (int i = 1; i <= 5; i++) {
+////            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+////            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+////            System.out.println("i = " + i);
+////        }
+//    }
+}

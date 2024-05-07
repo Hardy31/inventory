5301506b1673bc9d81e6931fd830bfe061af68d8
Hardy31 <alex-vav@list.ru>
2024-05-07 17:36:01 +0300

    conversion to UTF-8 CSV for correct display data in H2 and IJ

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+
+
+--INSERT INTO public.tmc(
+--    code, description, location,tag)
+--VALUES ('1380338', '5881 фон.рир.проекции', 'ТВ студия', 'баланс'),
+--       ('26627', 'DVD- рекордер Sony RDR-GX310', 'Склад', 'баланс'),
+--       ('1380432', 'DVD-рекордер', 'Склад', 'баланс'),
+--       ('1380331',	'FM-Процессор',	'ТРПЦ',	'баланс');
+
+--
+
+
+----
+ DROP TABLE IF EXISTS tmc;
+--
+-- CREATE TABLE tmc AS SELECT * FROM CSVREAD('resources/dataCSV01.csv');
+-- CREATE TABLE tmc AS SELECT * FROM CSVREAD('C:\Users\Administrator\IdeaProjects\inventory\src\main\resources\DataCSUU.csv',null,'fieldSeparator=;');
+
+-- CREATE TABLE tmc AS SELECT * FROM CSVREAD('C:/Users/Administrator/IdeaProjects/inventory/src/main/resources/dataCSVm.csv',null,'fieldSeparator=;');
+-- CREATE TABLE tmc AS SELECT * FROM CSVREAD('C:/Users/Administrator/IdeaProjects/inventory/src/main/resources/dataCSVt.csv',null,'fieldSeparator=;');
+-- CREATE TABLE tmc AS SELECT * FROM CSVREAD('C:/Users/Administrator/IdeaProjects/inventory/src/main/resources/dataCSVZ.csv',null,'fieldSeparator=;');
+-- CREATE TABLE tmc AS SELECT * FROM CSVREAD('C:/Users/Administrator/IdeaProjects/inventory/src/main/resources/TabUTF8.txt',null,'fieldSeparator=\\t');
+ CREATE TABLE tmc AS SELECT * FROM CSVREAD('C:/Users/Administrator/IdeaProjects/inventory/src/main/resources/encoded-111.csv',null,'fieldSeparator=;');
+-- CREATE TABLE tmc AS SELECT * FROM CSVREAD('resources/unicodeUTF8.txt',null,'fieldSeparator=;');
